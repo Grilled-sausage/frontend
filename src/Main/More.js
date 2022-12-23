59abdf154639f4f7f606de233caeaaebcbd575d8
dbstjrals <soekmin1998@naver.com>
2022-12-24 02:32:15 +0900

    Make More page

--- src/Main/More.js
+++ src/Main/More.js
@@ -1,5 +1,48 @@
-function More(){
+import MovieM from "../Components/MovieM";
+import { movieDummy } from "../movieDummy";
+import goMain from "../assets/goMain.svg";
+import './More.css';
+import { Link } from "react-router-dom";
 
+const boxStyle = {
+  width: "70%",
+  marginTop: "50px",
+  margin: "0",
+  backgroundColor: "#1d1d1d",
+  display: "grid",
+  marginLeft: "auto",
+  marginRight: "auto",
+  gridTemplateColumns: "repeat(auto-fill, minmax(255px, 1fr)",
+  placeItems: "center",
+  paddingTop: "70px"
+};
+
+function More({ genre }) {
+
+  return (
+    <>
+      <div>
+        <div className="more-info">
+          <Link to="/main">
+            <img src={goMain} />
+          </Link>
+        </div>
+        <h2 className="more-genre">전체</h2>
+      </div>
+
+      <div style={boxStyle}>
+        {movieDummy.results.map((item) => {
+          return (
+            <MovieM
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default More;
