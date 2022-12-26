ea4447088e8d6d43083580ff1a9a1f45c05d6169
hyeonseop2080 <gustjq2080@naver.com>
2022-12-26 16:49:22 +0900

    survey starrating

--- src/Components/MovieR.js
+++ src/Components/MovieR.js
@@ -2,14 +2,17 @@ import './MovieR.css';
 import React from 'react';
 import StarRate from './StarRate';
 
-function MovieR({title, poster_path}) {
+function MovieR({title, poster_path, id, func}) {
+  const setRating = (rating) => {
+    func({movieId: id, movieRating: rating});
+  }
 
   return (
     <>
       <div className='movieBox'>
         <img src={poster_path} alt='poster'></img>
         <h3>{title}</h3>
-        <StarRate size={30}/>
+        <StarRate size={30} func={setRating} />
       </div>
     </>
   );
--- src/Components/MovieRBox.js
+++ src/Components/MovieRBox.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, useRef} from "react";
 import MovieR from "./MovieR";
 import { SERVER_URL } from '../Components/Server';
 import axios from 'axios';
 
 
 function MovieRBox() {
+  let movieID;
   const [movies, setMovies] = useState([]);
+  const [rating, setRating] = useState({});
   useEffect(() => {
     axios.get(`${SERVER_URL}/api/content/survey/movie`, {
       headers: {
@@ -13,8 +15,28 @@ function MovieRBox() {
       }
     }).then((res) => {
       setMovies(res.data);
+      console.log(res.data);
     })
   }, []);
+
+  const useDidMountEffect = (func, deps) => {
+    const didMount = useRef(false);
+
+    useEffect(() => {
+        if (didMount.current) func();
+        else didMount.current = true;
+    }, deps);
+  }
+
+  useDidMountEffect(() => {
+    axios.post(`${SERVER_URL}/api/rating/movie`, rating, {
+      headers: {
+        Authorization: localStorage.getItem("Authorization")
+      }
+    }).then((res) => {
+      console.log(res.data);
+    })
+  }, [rating]);
 
   return (
     <>
@@ -36,6 +58,8 @@ function MovieRBox() {
             <MovieR
               title={item.name}
               poster_path={item.image}
+              id={item.id}
+              func={setRating}
             />
           );
         })}
