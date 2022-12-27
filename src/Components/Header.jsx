94f81ba186a19a6d64059d4176d1c6ceb115949d
dbstjrals <soekmin1998@naver.com>
2022-12-28 01:35:23 +0900

    Make New Intro Page & Fix Survey page Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,21 +7,23 @@ import "./Header.css";
 export default function Header() {
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <img src={mainLogo} alt="logo" />
-        </div>
+      <div className='navbar-container'>
+        <nav className="intro-navbar">
+          <div className="navbar-helper">
+            <img src={mainLogo} alt="logo" />
+          </div>
 
-        <div className="navbar-title">
-          평가하기
-        </div>
+          <div className="navbar-title">
+            평가하기
+          </div>
 
-        <div className="navbar-submit">
-          <Link className="main-link" to="/main">
-            완료
-          </Link>
-        </div>
-      </nav>
+          <div className="navbar-submit">
+            <Link className="main-link" to="/main">
+              완료
+            </Link>
+          </div>
+        </nav>
+      </div>
     </>
   );
 
--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -2,26 +2,24 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import introLogo from "./intro-logo.svg";
 import React from 'react';
+import introLogin from '../assets/introLogo.svg';
 
 import "./Intro.css"
 
-function Intro(){
-    return (
-        <div className="container">
-            <div className="header">
-                <ul>
-                    <li className="login-li">
-                        <Link className="login-link" to="/login">
-                            <span>로그인</span>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="body">
-                <img className="logo" src={introLogo}/>
-            </div>
+function Intro() {
+  return (
+    <div className="container">
+      <div className="body">
+        <img className="logo" src={introLogo} />
+        <div className="intro-title">
+          <h2>당신을 위한 영화 추천 및 평가 사이트 </h2>
         </div>
-    );
+        <Link className="login-link" to="/login">
+          <img src={introLogin} />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Intro;
